31dad90f52c5781c450dab720987d754e7238c7a
Daniel S <daniel.solisb@outlook.com>
2021-06-18 16:57:08 -0500

    nuevo commit ds

--- Market_PI/gestionPedidos/admin.py
+++ Market_PI/gestionPedidos/admin.py
@@ -3,6 +3,24 @@ from .models import Clientes, Articulos, Pedidos
 
 # Register your models here.
 
-admin.site.register(Clientes)
-admin.site.register(Articulos)
-admin.site.register(Pedidos)
+#clase para mostrar y modificar el model.admin
+class ClientesAdmin(admin.ModelAdmin):
+    #cammpo para mostrar en lista de admin
+    list_display=("nombre","direccion","tfno")
+    #campo para colocar casilla de busqueda y especificar q campos buscar
+    search_fields=("nombre","tfno")
+
+class ArticulosAdmin(admin.ModelAdmin):
+    #campo para mostrar en lista de admin
+    list_display=("nombre", "seccion", "precio")
+    #campo para agregar filtro en el panel admin
+    list_filter=("seccion",)
+
+class PedidosAdmin(admin.ModelAdmin):
+    list_display=("numero","fecha","entregado")
+    list_filter=("fecha",)
+    date_hierarchy="fecha"
+
+admin.site.register(Clientes, ClientesAdmin)
+admin.site.register(Articulos, ArticulosAdmin)
+admin.site.register(Pedidos, PedidosAdmin)
--- Market_PI/gestionPedidos/models.py
+++ Market_PI/gestionPedidos/models.py
@@ -3,11 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Clientes(models.Model):
-    nombre = models.CharField(max_length=30)
-    direccion = models.CharField(max_length=50)
-    email = models.EmailField()
+    nombre = models.CharField(max_length=30, verbose_name='Su Nombre')
+    direccion = models.CharField(max_length=50, verbose_name='Dirección')
+    email = models.EmailField(blank=True, null=True)
     tfno = models.CharField(max_length=15)
-
+    #para mostrar campos, no se puede mostrar mas de un campo
+    #si se desea puede revisar en admin.py la clase creada para elloS
     def __str__(self):
         return self.nombre
 
@@ -15,6 +16,7 @@ class Articulos(models.Model):
     nombre = models.CharField(max_length=30)
     seccion = models.CharField(max_length=30)
     precio = models.IntegerField()
+
     def __str__(self):
         return self.nombre
 
@@ -22,6 +24,6 @@ class Pedidos(models.Model):
     numero = models.IntegerField()
     fecha = models.DateField()
     entregado = models.BooleanField()
-    def __str__(self):
-        return self.numero
+
+
 
